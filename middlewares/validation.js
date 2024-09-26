9ed4922a131eb89ee046f20d275506e25cdcb247
Clay Schurwon <clayschurwon@yahoo.com>
2024-09-26 11:51:41 -0500

    Sprint 15 updates attempt 2

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -9,6 +9,20 @@ const validateURL = (value, helpers) => {
   return helpers.error("string.uri");
 };
 
+// Validator for checking 'name' and 'avatar' fields
+module.exports.validateAvatarAndName = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30).messages({
+      "string.min": 'The minimum length of the "name" field is 2',
+      "string.max": 'The maximum length of the "name" field is 30',
+      "string.empty": 'The "name" field must be filled in',
+    }),
+    avatar: Joi.string().required().custom(validateURL).messages({
+      "string.empty": 'The "avatar" field must be filled in',
+      "string.uri": 'The "avatar" field must be a valid URL',
+    }),
+  }),
+});
 // Validator for creating a clothing item
 module.exports.validateClothingItemBody = celebrate({
   body: Joi.object().keys({
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,22 @@
 const router = require("express").Router();
 const { auth } = require("../middlewares/auth");
-const { validateUserBody } = require("../middlewares/validation");
-const { getCurrentUser } = require("../controllers/users");
+const {
+  validateUserBody, // For signup
+  validateAvatarAndName, // For profile updates
+} = require("../middlewares/validation");
+const {
+  getCurrentUser,
+  updateCurrentUser,
+  createUser, // Assuming this is for signup
+} = require("../controllers/users");
 
 // Route for getting the current user info
 router.get("/me", auth, getCurrentUser);
 
-// Route for updating the current user's information
-router.patch("/me", auth, validateUserBody);
+// Route for updating the current user's information (name and avatar only)
+router.patch("/me", auth, validateAvatarAndName, updateCurrentUser);
+
+// Route for signing up a new user (requires name, avatar, email, and password)
+router.post("/signup", validateUserBody, createUser);
 
 module.exports = router;
